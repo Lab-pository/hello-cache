b64a4e17c57af703475ff6764b9ff3786e8a7be4
SeoSeunghun <proto_seo@naver.com>
2024-04-23 20:03:06 +0900

    refactor: default connectionFactory 사용하도록 수정

--- src/main/java/com/example/hellocache/config/RedisConfig.java
+++ src/main/java/com/example/hellocache/config/RedisConfig.java
@@ -1,9 +1,11 @@
 package com.example.hellocache.config;
 
-import static org.springframework.data.redis.serializer.RedisSerializationContext.SerializationPair.*;
+import static org.springframework.data.redis.serializer.RedisSerializationContext.SerializationPair.fromSerializer;
 
 import java.time.Duration;
-import org.springframework.beans.factory.annotation.Value;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
@@ -11,8 +13,6 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.cache.RedisCacheConfiguration;
 import org.springframework.data.redis.cache.RedisCacheManager;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
-import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
-import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
@@ -21,31 +21,11 @@ import org.springframework.data.redis.serializer.StringRedisSerializer;
 @Configuration
 public class RedisConfig {
 
-    @Value("${spring.data.redis.host}")
-    public String host;
-
-    @Value("${spring.data.redis.port}")
-    public int port;
-
-    @Value("${spring.data.redis.username}")
-    public String username;
-
-    @Value("${spring.data.redis.password}")
-    public String password;
+    private final Logger log = LoggerFactory.getLogger(getClass());
 
     @Bean
-    public LettuceConnectionFactory redisConnectionFactory() {
-        final RedisStandaloneConfiguration redisStandaloneConfiguration = new RedisStandaloneConfiguration();
-        redisStandaloneConfiguration.setHostName(host);
-        redisStandaloneConfiguration.setPassword(password);
-        redisStandaloneConfiguration.setUsername(username);
-        redisStandaloneConfiguration.setPassword(password);
-        return new LettuceConnectionFactory(redisStandaloneConfiguration);
-    }
-
-    @Bean
-    public RedisTemplate<String, Object> redisTemplate(
-        final RedisConnectionFactory connectionFactory) {
+    public RedisTemplate<String, Object> redisTemplate(final RedisConnectionFactory connectionFactory) {
+        log.info("{}", connectionFactory.getClass());
         RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
 
         redisTemplate.setKeySerializer(new StringRedisSerializer());
@@ -58,12 +38,12 @@ public class RedisConfig {
     @Bean
     public CacheManager redisCacheManager(final RedisConnectionFactory redisConnectionFactory) {
         RedisCacheConfiguration redisCacheConfiguration = RedisCacheConfiguration.defaultCacheConfig()
-            .serializeKeysWith(fromSerializer(new StringRedisSerializer()))
-            .serializeValuesWith(fromSerializer(new GenericJackson2JsonRedisSerializer()))
-            .entryTtl(Duration.ofMinutes(1L));
+                .serializeKeysWith(fromSerializer(new StringRedisSerializer()))
+                .serializeValuesWith(fromSerializer(new GenericJackson2JsonRedisSerializer()))
+                .entryTtl(Duration.ofMinutes(1L));
 
         return RedisCacheManager.RedisCacheManagerBuilder.
-            fromConnectionFactory(redisConnectionFactory).cacheDefaults(redisCacheConfiguration)
-            .build();
+                fromConnectionFactory(redisConnectionFactory).cacheDefaults(redisCacheConfiguration)
+                .build();
     }
 }
